687a3c85d86d43384e9c8d90e33f906682dc02c7
28716642 <28716642@ppti-14-507-03.ufr-info-p6.jussieu.fr>
2020-10-20 18:47:02 +0200

    Test to fix

--- Java/src/com/paracamplus/ilp1/ilp1tme1/test/TestCompiler.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme1/test/TestCompiler.java
@@ -2,12 +2,16 @@ package com.paracamplus.ilp1.ilp1tme1.test;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 
+import org.junit.Test;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.paracamplus.ilp1.compiler.CompilationException;
 import com.paracamplus.ilp1.compiler.test.CompilerRunner;
 import com.paracamplus.ilp1.compiler.test.CompilerTest;
+import com.paracamplus.ilp1.parser.ParseException;
 
 public class TestCompiler extends CompilerTest{
     public static String[] samplesDirName = { "SamplesTME1" };
@@ -19,4 +23,10 @@ public class TestCompiler extends CompilerTest{
     	return CompilerRunner.getFileList(samplesDirName, pattern);
     }    	
 	
+	@Test
+    public void processFile() throws CompilationException, ParseException, IOException {
+    	CompilerRunner run = new CompilerRunner();
+    	configureRunner(run);
+        run.checkPrintingAndResult(file, run.compileAndRun(file));	
+    }
 }
--- Java/src/com/paracamplus/ilp1/ilp1tme1/test/TestInterprete.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme1/test/TestInterprete.java
@@ -2,12 +2,16 @@ package com.paracamplus.ilp1.ilp1tme1.test;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 
+import org.junit.Test;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.test.InterpreterRunner;
 import com.paracamplus.ilp1.interpreter.test.InterpreterTest;
+import com.paracamplus.ilp1.parser.ParseException;
 
 public class TestInterprete extends InterpreterTest{
     protected static String[] samplesDirName = { "SamplesTME1" }; 
@@ -19,6 +23,13 @@ public class TestInterprete extends InterpreterTest{
 	public static Collection<File[]> data() throws Exception {
     	return InterpreterRunner.getFileList(samplesDirName, pattern);
     }    
+	@Test
+    public void processFile() throws EvaluationException, ParseException, IOException {
+    	InterpreterRunner run = new InterpreterRunner();
+    	configureRunner(run);
+    	run.testFile(file);
+    	run.checkPrintingAndResult(file);
+    }
 
 	
 
